6b2d425fc6234a19b48abcc1d456a1d867b0efe3
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2026-01-25 23:01:47 +0100

    add some kind of validation

--- systems/modification/modification.gd
+++ systems/modification/modification.gd
@@ -1,6 +1,13 @@
+@tool
 extends Resource
 class_name Modification
 
+static var expected_types = {
+		ModifiableProperty.WeaponPropertyKey.TRAJECTORY_STRATEGY: TrajectoryStrategy,
+		ModifiableProperty.WeaponPropertyKey.SPAWN_STRATEGY: SpawnStrategy,
+		ModifiableProperty.WeaponPropertyKey.TARGET_STRATEGY: TargetStrategy,
+		# TODO: Add other strategy types here as needed
+	}
 enum ModificationType {
 	ADD,
 	MULTIPLY,
@@ -15,4 +22,17 @@ enum ModificationType {
 @export var stat_operation: ModificationType
 @export var target_prop: ModifiableProperty.WeaponPropertyKey
 @export var stat_value: float
-@export var replacement_strategy: Resource
+@export var replacement_strategy: Resource:
+	set(value):
+		if not _validate_strategy_type(value):
+			push_error("Invalid strategy type assigned to modification for property %s" % str(target_prop))
+			return
+		replacement_strategy = value
+
+
+func _validate_strategy_type(strategy: Resource) -> bool:
+	if target_prop not in expected_types:
+		return true
+		
+	var expected_type = expected_types[target_prop]
+	return is_instance_of(strategy, expected_type)
